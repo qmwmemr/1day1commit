6055af205849eae13de29e2fd41f4f574246bfb3
kim <qmwmemr@naver.com>
2021-08-07 17:40:46 +0900

    08/07

--- src/exam30/exam39.java
+++ src/exam30/exam39.java
@@ -24,21 +24,6 @@ public class exam39 {
 		for(int i =0; i<n; i++) {
 			arr[i] = scan.nextInt();
 		}
-
-		for(int i = 1; i<n; i++) {
-			int tmp = arr[i], j;
-			for(j =i-1; j>=0;j--) {
-				if(arr[j]>tmp) {
-					arr[j+1] = arr[j];
-					
-				}else {
-					break;
-				}
-			}
-			arr[j+1] = tmp;
-		}
-		
-		System.out.println(Arrays.toString(arr));
 		
 	}
 	}	
