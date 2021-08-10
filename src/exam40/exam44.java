5336eed4cb4b26aeb00c39b380cba618b37a8e87
kim <qmwmemr@naver.com>
2021-08-10 15:16:49 +0900

    08/10

--- src/exam40/exam44.java
+++ src/exam40/exam44.java
@@ -28,29 +28,8 @@ public class exam44 {
 			arr[i] = scan.nextInt();
 		}
 		
-		int answer = 0;
-		
-		Arrays.sort(arr);
-		
-		int lt =0, rt=n-1;
-		
-		while(lt<=rt) {
-			int mid = (lt+rt)/2;
-			if(arr[mid] == m) {
-				answer = mid+1;
-				break;
-			}
-			if(arr[mid] > m){
-					rt = mid-1;
-			}else {
-				lt = mid+1;
-			
-			}
-		}
 		
 		
-		
-		System.out.println(answer);
 	}
 	}	
 
